157bff67deb37dbbd0dbc2200e285e1fb7ad7f91
zalwan <rizalsuryawan86@gmail.com>
2025-08-10 23:19:44 +0700

    feat: add route for delete

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,4 +5,8 @@ app_name = "dashboard"
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('delete-history/', views.delete_all_history, name='delete_all_history'),
+    path('delete-attempt/<int:attempt_id>/',
+         views.delete_attempt, name='delete_attempt'),
+
 ]
